632b1226ca63d1f0f8caa112578722bcfe218ab2
seokhwa <stone2f@gmail.com>
2022-06-24 15:44:04 +0900

    second commit

--- pipdisttest/test.py
+++ pipdisttest/test.py
@@ -2,3 +2,8 @@ def test_function(input_str):
     print("Hello :)")
     print("your input string is {}".format(input_str))
     print("Bye~~")
+
+model_path = 't3q_folder/model_path'
+
+T3QAI_TRAIN_OUTPUT_PATH = os.path.join(model_path,'t3qai_output')
+T3QAI_TRAIN_MODEL_PATH = model_path
